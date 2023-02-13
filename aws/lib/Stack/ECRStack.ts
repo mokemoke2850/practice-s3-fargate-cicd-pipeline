68fea9683920a38ba6cdd5097e02dcea9d1c2111
shoma <koalakoala.xxx@gmail.com>
2023-02-13 23:06:48 +0900

    fixed ECRStack

--- aws/lib/Stack/ECRStack.ts
+++ aws/lib/Stack/ECRStack.ts
@@ -1,15 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
+import path = require('path');
+import * as imagedeploy from 'cdk-docker-image-deployment';
 
 export class ECRStack extends cdk.Stack {
   public readonly repository: ecr.Repository;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    this.repository = new ecr.Repository(scope, 'backend-repository', {
+    this.repository = new ecr.Repository(this, 'backend-repository', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       repositoryName: 'github-actions-test',
+    });
+
+    // 作成したECRへイメージをプッシュ;
+    new imagedeploy.DockerImageDeployment(this, 'initial-image-push', {
+      source: imagedeploy.Source.directory(
+        path.join(__dirname, '../../../', 'backend')
+      ),
+      destination: imagedeploy.Destination.ecr(this.repository, {
+        tag: 'latest',
+      }),
     });
   }
 }
