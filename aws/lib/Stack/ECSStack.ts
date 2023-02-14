3afdde7a6f7abae87e976b5848d21d2b4f5b7723
shoma <koalakoala.xxx@gmail.com>
2023-02-14 23:32:32 +0900

    added the ECSStack

---
+++ aws/lib/Stack/ECSStack.ts
@@ -0,0 +1,64 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
+
+export class ECSStack extends cdk.Stack {
+  public readonly ecsService: ecsp.ApplicationLoadBalancedFargateService;
+
+  constructor(
+    scope: cdk.Stack,
+    id: string,
+    vpc: cdk.aws_ec2.Vpc,
+    alb: cdk.aws_elasticloadbalancingv2.ApplicationLoadBalancer,
+    repository: cdk.aws_ecr.IRepository,
+    frontendURL: string,
+    props?: cdk.StackProps
+  ) {
+    super(scope, id, props);
+
+    const ecsCluster = new ecs.Cluster(scope, 'backend-api-ecs-cluster', {
+      clusterName: 'github-actions-test-cluster',
+      vpc: vpc,
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(
+      scope,
+      'backend-api-fargate-task',
+      {
+        cpu: 256,
+        memoryLimitMiB: 512,
+      }
+    );
+    taskDefinition.addContainer('backend-api-container', {
+      containerName: 'backend-api',
+      image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
+      portMappings: [{ containerPort: 80, hostPort: 80 }],
+      environment: {
+        FRONTEND_URL: frontendURL,
+        API_ENV: 'production',
+        API_PORT: '80',
+      },
+      healthCheck: {
+        command: [
+          'CMD-SHELL',
+          'curl -f http://localhost:80/ishealthy || exit 1',
+        ],
+      },
+    });
+
+    this.ecsService = new ecsp.ApplicationLoadBalancedFargateService(
+      scope,
+      'ecsp-alb-fargate',
+      {
+        cluster: ecsCluster,
+        taskDefinition,
+        serviceName: 'github-actions-test-service',
+        loadBalancer: alb,
+        desiredCount: 1,
+        taskSubnets: vpc.selectSubnets({
+          subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        }),
+      }
+    );
+  }
+}
--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -3,6 +3,12 @@ import { Construct } from 'constructs';
 import { S3Stack } from './Stack/S3Stack';
 import { VpcStack } from './Stack/VpcStack';
 import { AlbStack } from './Stack/AlbStack';
+import { ECRStack } from './Stack/ECRStack';
+import * as imagedeploy from 'cdk-docker-image-deployment';
+import { EcsTask } from 'aws-cdk-lib/aws-events-targets';
+import { ECSStack } from './Stack/ECSStack';
+import path = require('path');
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,6 +16,7 @@ export class AwsStack extends cdk.Stack {
 
     // フロントエンド用バケット
     const frontendBucket = new S3Stack(this, 'frontend-stack', props);
+    const frontendURL = `http://${frontendBucket.bucket.bucketDomainName}`;
 
     // ECS配置用VPC
     const vpc = new VpcStack(this, 'ecs-vpc-stack', props);
@@ -17,8 +24,28 @@ export class AwsStack extends cdk.Stack {
     // ECS用ALB
     const alb = new AlbStack(this, 'ecs-alb-stack', vpc.vpc, props);
 
+    const testrepository = ecr.Repository.fromRepositoryName(
+      this,
+      'test-repository',
+      'hogehoge'
+    );
+
+    // バックエンド用ECR
+    const repository = new ECRStack(this, 'ecr-stack', props);
+
+    // バックエンド用Fargate
+    const ecs = new ECSStack(
+      this,
+      'ecs-fargate-stack',
+      vpc.vpc,
+      alb.alb,
+      repository.repository,
+      frontendURL,
+      props
+    );
+
     new cdk.CfnOutput(this, 'S3Domain', {
-      value: `http://${frontendBucket.bucket.bucketDomainName}`,
+      value: frontendURL,
     });
 
     new cdk.CfnOutput(this, 'ALBDomain', {
